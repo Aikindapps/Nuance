d47b7938090adbf10c77d1fb861f6974fac05cb1
Mitch <mitchellkurtzman.aiken@gmail.com>
2023-12-07 19:19:54 -0500

    updated sitemap

--- scripts/BuildSitemap.sh
+++ scripts/BuildSitemap.sh
@@ -6,7 +6,6 @@ data=$(cat scripts/url-list.txt)
 # Remove unwanted characters and format URLs, one per line
 cleaned_data=$(echo -e "$data" | tr -d '",' )
 
-
 # Write cleaned data back to a temporary file
 echo "$cleaned_data" > scripts/temp-url-list.txt
 
@@ -24,14 +23,15 @@ fi
 # Create sitemap.xml file
 touch src/SEO/sitemap.xml
 
-# Add XML header to the file
-echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" >> src/SEO/sitemap.xml
+# Add extended XML header to the file
+echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:news=\"http://www.google.com/schemas/sitemap-news/0.9\" xmlns:xhtml=\"http://www.w3.org/1999/xhtml\" xmlns:image=\"http://www.google.com/schemas/sitemap-image/1.1\" xmlns:video=\"http://www.google.com/schemas/sitemap-video/1.1\">" >> src/SEO/sitemap.xml
 
-# Loop through the list of URLs and add each URL to the sitemap.xml file
+# Loop through the list of URLs and add each URL to the sitemap.xml file with changefreq and priority
 while read -r line; do
   echo "  <url>" >> src/SEO/sitemap.xml
   echo "    <loc>https://nuance.xyz$line</loc>" >> src/SEO/sitemap.xml
-  echo "    <lastmod>"$(date +%Y-%m-%d)"</lastmod>" >> src/SEO/sitemap.xml
+  echo "    <changefreq>weekly</changefreq>" >> src/SEO/sitemap.xml
+  echo "    <priority>0.5</priority>" >> src/SEO/sitemap.xml
   echo "  </url>" >> src/SEO/sitemap.xml
 done < scripts/temp-url-list.txt
 
