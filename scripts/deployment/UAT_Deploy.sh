82d513f3fadfabd701d9f71b2ed9f0164cb1da77
Mitchell Kurtzman <68413758+MITCHELLKURTZMAN@users.noreply.github.com>
2023-12-08 14:06:56 -0500

    Update UAT_Deploy.sh

--- scripts/deployment/UAT_Deploy.sh
+++ scripts/deployment/UAT_Deploy.sh
@@ -11,7 +11,7 @@ echo "Deploying nuance main repo to UAT network $NETWORK..."
 echo ""
 
 dfx generate
-dfx deploy --network $NETWORK
+dfx deploy --network $NETWORK -y
 node scripts/upgrades/bucketCanisterUpgrade.js --multi --$NETWORK
 
 echo ""
