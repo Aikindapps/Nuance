61fd29ffe63ee8cb23786a6bbcc79b5a37106901
Mitch <mitchellkurtzman.aiken@gmail.com>
2023-12-24 09:51:53 -0500

    testing vote no

--- scripts/sns/vote_no.sh
+++ scripts/sns/vote_no.sh
@@ -19,10 +19,17 @@ quill_register_vote_base="quill sns register-vote --vote n --canister-ids-file .
 quill_send="quill send --yes $NETWORK --pem-file ~/.config/dfx/identity/$IDENTITY/identity.pem"
 
 # Array of proposal IDs
-proposal_ids=("1") # Remember to check the previous upgrade prior to voting on the next upgrade
+proposal_ids=("22") # Remember to check the previous upgrade prior to voting on the next upgrade
 
 # Array of vote IDs
-vote_ids=("a0e3889b406b7686640619648d848d5a0d800f2b5a7a2a44ff7cda7b2d264131") # Add your vote IDs here
+
+##comment back for full vote:
+#vote_ids=("a0e3889b406b7686640619648d848d5a0d800f2b5a7a2a44ff7cda7b2d264131")
+
+#use for testing smaller portion of vote
+vote_ids=("d9abade7010df83a341296933d74ddacdbe6025dd059b00d4590dc9ed2733ac7") 
+
+
 
 # Loop through each proposal ID and vote ID
 for proposal_id in "${proposal_ids[@]}"; do
