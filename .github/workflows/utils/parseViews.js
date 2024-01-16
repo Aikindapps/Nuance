9c90ff7423ae846c0bf85a40e6eb64020a99b5df
Mitch <mitchellkurtzman.aiken@gmail.com>
2024-01-16 13:11:44 -0500

    debugging

--- .github/workflows/utils/parseViews.js
+++ .github/workflows/utils/parseViews.js
@@ -1,17 +1,17 @@
 // parseViews.js
 const rawData = process.argv[2];
 
-
 function parseViews(data) {
   const regex = /(\d+) : nat/;
   const match = regex.exec(data);
 
   if (match && match[1]) {
     const totalViews = parseInt(match[1], 10);
-    return `Total Views: ${totalViews}`;
+    return totalViews.toString(); // Output as a string
   } else {
-    return 'No valid views metric data found.';
+    return '0'; 
   }
 }
 
-const analysisMetrics = parseViews(rawData);
+const totalViews = parseViews(rawData);
+console.log(totalViews); 
