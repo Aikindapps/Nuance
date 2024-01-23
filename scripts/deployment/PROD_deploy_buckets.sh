f99ba10c61a37290baa5349aa464a29dbcf2d455
Mitch <mitchellkurtzman.aiken@gmail.com>
2024-01-23 14:32:07 -0500

    Remove asset canister from bucket deployment scripts

--- scripts/deployment/PROD_deploy_buckets.sh
+++ scripts/deployment/PROD_deploy_buckets.sh
@@ -7,20 +7,14 @@ echo $PUBLICATIONS_REPO_PATH
 echo $NUANCE_MAIN_REPO_PATH
 
 echo ""
-echo "Deploying frontend to PROD network $NETWORK..."
+echo "Deploy Post Buckets Network $NETWORK..."
 echo ""
 
-dfx generate
-dfx build --all --network $NETWORK
-dfx deploy nuance_assets --network $NETWORK
 node scripts/upgrades/bucketCanisterUpgrade.js --multi --$NETWORK
 
 echo ""
-echo "Deploying nuance publication buckets to PROD network $NETWORK..."
+echo "Deploying Publication Buckets to PROD Network $NETWORK..."
 echo ""
 
 cd $PUBLICATIONS_REPO_PATH
-dfx generate
-dfx build --all --network $NETWORK
-
 node scripts/upgrade-publication-canisters.js  --multi --$NETWORK
