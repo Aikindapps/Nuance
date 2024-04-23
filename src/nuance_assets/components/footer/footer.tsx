14b85b330c41a37299e4d43e1a000eafa96540ff
Mitch <mitchellkurtzman.aiken@gmail.com>
2024-04-23 13:33:15 -0400

    Aikin removed from the footer

--- src/nuance_assets/components/footer/footer.tsx
+++ src/nuance_assets/components/footer/footer.tsx
@@ -4,17 +4,8 @@ import { images } from '../../shared/constants';
 const Footer = () => {
   return (
     <div className='footer-image-section'>
-      <span className='logo-header1'>Built by:</span>
       <span className='logo-header2'>Powered by:</span>
       <div className='footer-image-container'>
-        <a target='_blank' href='https://www.aikin.io'>
-          <img
-            className='footer-image'
-            src={images.AIKIN_LOGO_HORIZONTAL}
-            alt='The Aikin logo'
-            loading='lazy'
-          />
-        </a>
         <a target='_blank' href='https://www.dfinity.org'>
           <img
             className='footer-image'
